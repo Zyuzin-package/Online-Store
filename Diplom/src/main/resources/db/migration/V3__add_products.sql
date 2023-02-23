c782f7838780d172a93894cc31acb395d0bb30ed
Zyuzin-package <zyuzinm4@gmail.com>
2023-02-23 16:02:45 +0400

    add images for product

--- Diplom/src/main/resources/db/migration/V3__add_products.sql
+++ Diplom/src/main/resources/db/migration/V3__add_products.sql
@@ -1,10 +1,10 @@
 INSERT INTO products (id, price, title, image, description)
-VALUES (11, 450.0, 'Meat1', '/img/gof78man.jpg', 'description'),
-       (22, 45.0, 'Meat2', '/img/gof78man.jpg', 'description'),
-       (33, 65.0, 'Cheese1', '/img/gof78man.jpg', 'description'),
-       (44, 115.0, 'Cheese2', '/img/gof78man.jpg', 'description'),
-       (55, 58.0, 'Spice1', '/img/gof78man.jpg', 'description'),
-       (66, 58.0, 'Spice2', '/img/gof78man.jpg', 'description'),
+VALUES (11, 450.0, 'Meat1', '/img/1.jpg', 'description'),
+       (22, 45.0, 'Meat2', '/img/2.jpg', 'description'),
+       (33, 65.0, 'Cheese1', '/img/3.jpg', 'description'),
+       (44, 115.0, 'Cheese2', '/img/4.jpg', 'description'),
+       (55, 58.0, 'Spice1', '/img/5.jpg', 'description'),
+       (66, 58.0, 'Spice2', '/img/6.jpg', 'description'),
        (78, 78, 'Gofman', '/img/gof78man.jpg',
         'Мелкий предприниматель еврейской национальности из города Херсон, ставший известным благодаря своим видеороликам, в которых рассказывает о «своей биографии в контексте истории развития Земли и мироздания в целом, их взаимопереплетении и взаимовлиянии».');
 ALTER SEQUENCE product_seq RESTART WITH 90;
--- Diplom/src/main/resources/db/migration/V4__add_categories.sql
+++ Diplom/src/main/resources/db/migration/V4__add_categories.sql
@@ -1,6 +1,5 @@
 INSERT INTO products_categories (product_id, category_id)
 VALUES (11, 1),
-       (11, 3),
        (22, 1),
        (33, 2),
        (44, 2),
