912e5181090fd16aeee530245c9f4a406191b24f
Zyuzin-package <zyuzinm4@gmail.com>
2023-03-05 01:35:03 +0400

    fix forbiden url for user

--- Diplom/src/main/java/com/example/diplom/config/SecurityConfig.java
+++ Diplom/src/main/java/com/example/diplom/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/admin/**").hasAuthority("ADMIN")
                 .antMatchers("/my/**").authenticated()
                 .antMatchers("/order/**").authenticated()
                 .antMatchers("/registration").not().fullyAuthenticated()
