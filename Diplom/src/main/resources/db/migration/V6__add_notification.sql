df0ebb9c5c6cd5d5f450b6ade01e6cddd875bd33
Zyuzin-package <zyuzinm4@gmail.com>
2023-03-14 14:35:29 +0400

    rework notifications sql querry

--- Diplom/src/main/resources/db/migration/V6__add_notification.sql
+++ Diplom/src/main/resources/db/migration/V6__add_notification.sql
@@ -1,10 +1,10 @@
 Insert Into user_notification (id, message, url, url_text, user_id)
 VALUES
     (1,'Product: MEAT was added to you bucket','','',1),
-    (2,'TEST','','',1),
-    (3,'TEST','','',1),
-    (4,'TEST','','',1),
-    (5,'TEST','','',1)
+    (2,'Order with number 1 was created','','',1),
+    (3,'Product: CHEESE was added to you bucket','','',1),
+    (4,'Order with number 78 was created','','',1),
+    (5,'Order with number 56 was created','','',1)
 
 ;
 ALTER SEQUENCE user_notification_seq RESTART with 8;
