ccba2093f2c4d0b69ea7b886a70dec70c0c938bd
Zyuzin-package <zyuzinm4@gmail.com>
2023-07-17 21:00:22 +0400

    remove

--- StatsHandler/src/main/java/com/example/statshandler/domain/OrderStatus.java
+++
@@ -1,6 +0,0 @@
-package com.example.statshandler.domain;
-
-public enum OrderStatus {
-    NEW,APPROVED,CANCELED,PAID,CLOSED,COMPLETED;
-
-}
--- StatsHandler/src/main/java/com/example/statshandler/domain/Role.java
+++
@@ -1,5 +0,0 @@
-package com.example.statshandler.domain;
-
-public enum Role {
-    CLIENT,MANAGER,ADMIN
-}
