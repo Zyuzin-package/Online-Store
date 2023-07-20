569df750cbce5bdec6aa75afab379cc7d474fa59
Zyuzin-package <zyuzinm4@gmail.com>
2023-07-20 19:04:17 +0400

    model for transfert data

---
+++ StatsHandler/src/main/java/com/example/statshandler/model/StatsDTO.java
@@ -0,0 +1,26 @@
+package com.example.statshandler.model;
+
+import com.example.models.dto.ProductDTO;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class StatsDTO {
+    private Long id;
+    private ProductDTO product;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime created;
+}
