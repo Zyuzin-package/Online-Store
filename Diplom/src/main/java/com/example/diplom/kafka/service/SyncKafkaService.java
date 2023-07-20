d2a7cb01580f7eed7dd3f44e2d33aa1d8638cd1e
Zyuzin-package <zyuzinm4@gmail.com>
2023-07-20 19:12:15 +0400

    added types

--- Diplom/src/main/java/com/example/diplom/kafka/service/SyncKafkaService.java
+++ Diplom/src/main/java/com/example/diplom/kafka/service/SyncKafkaService.java
@@ -9,6 +9,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
 
-public interface SyncKafkaService {
-    String get(List<VisitStatsDTO> visitStatsDTOS) throws TimeoutException;
+public interface SyncKafkaService<T> {
+    String get(List<T> t) throws TimeoutException;
 }
