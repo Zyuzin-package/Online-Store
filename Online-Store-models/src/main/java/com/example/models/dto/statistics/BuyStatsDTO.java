cab785a4cbd2e8d6867af9e7f411c5ad7f0cd15f
Zyuzin-package <zyuzinm4@gmail.com>
2023-07-20 19:07:18 +0400

    jackson annotations

--- Online-Store-models/src/main/java/com/example/models/dto/statistics/BuyStatsDTO.java
+++ Online-Store-models/src/main/java/com/example/models/dto/statistics/BuyStatsDTO.java
@@ -3,6 +3,11 @@ package com.example.models.dto.statistics;
 
 import com.example.models.domain.statistics.BuyStats;
 import com.example.models.dto.ProductDTO;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -16,6 +21,9 @@ public class BuyStatsDTO {
     private Long id;
     private ProductDTO product;
     private int amount;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime created;
 
     public BuyStatsDTO(BuyStats buyStats) {
