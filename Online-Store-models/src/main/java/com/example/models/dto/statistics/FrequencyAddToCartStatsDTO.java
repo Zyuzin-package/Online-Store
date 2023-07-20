0d2570819ec46cedfa5c73ed4db2be532e935cda
Zyuzin-package <zyuzinm4@gmail.com>
2023-07-20 19:07:37 +0400

    jackson annotations

--- Online-Store-models/src/main/java/com/example/models/dto/statistics/FrequencyAddToCartStatsDTO.java
+++ Online-Store-models/src/main/java/com/example/models/dto/statistics/FrequencyAddToCartStatsDTO.java
@@ -1,6 +1,12 @@
 package com.example.models.dto.statistics;
 
 import com.example.models.domain.Product;
+import com.example.models.dto.ProductDTO;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -14,7 +20,10 @@ public class FrequencyAddToCartStatsDTO {
 
     private Long id;
 
-    private Product product;
+    private ProductDTO product;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime created;
 
     public Long getId() {
@@ -25,11 +34,7 @@ public class FrequencyAddToCartStatsDTO {
         this.id = id;
     }
 
-    public Product getProduct() {
-        return product;
-    }
-
-    public void setProduct(Product product) {
+    public void setProduct(ProductDTO product) {
         this.product = product;
     }
 
